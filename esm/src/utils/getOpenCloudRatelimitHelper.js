592d4ddf12990534c60be3eed9fcfed0e4d11e8e
Julli4n <11952329+Julli4n@users.noreply.github.com>
2022-03-30 01:30:20 +0000

    Build: (ee53b3c) Disable OpenCloud datastores ratelimits temporarily

--- esm/src/utils/getOpenCloudRatelimitHelper.js
+++ esm/src/utils/getOpenCloudRatelimitHelper.js
@@ -15,70 +15,107 @@ export function getOpenCloudRatelimitHelper(rest) {
             duration: 60000,
             count: 10,
         },
+        /*
+        This is temporarily disabled.
+        TODO: the OpenCloud rate limit helpers were not made to do throughput rateliiting, and per universe ratelimits.
+        During the private beta, it was per API key.
+
         // DataStore APIs
         // List DataStore entries
         {
             methods: ["GET"],
-            pattern: new dntShim.URLPattern(rest.formatUrl("{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries?*")),
+            pattern: new URLPattern(
+                rest.formatUrl(
+                    "{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries?*",
+                ),
+            ),
             limitations: ["Authenticated"],
-            duration: 60000,
+            duration: 60_000,
             count: 300,
         },
         // Get the value of a DataStore entry
         {
             methods: ["GET"],
-            pattern: new dntShim.URLPattern(rest.formatUrl("{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries/entry?*")),
+            pattern: new URLPattern(
+                rest.formatUrl(
+                    "{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries/entry?*",
+                ),
+            ),
             limitations: ["Authenticated"],
-            duration: 60000,
+            duration: 60_000,
             count: 300,
         },
         // Update the value of a DataStore entry
         {
             methods: ["POST"],
-            pattern: new dntShim.URLPattern(rest.formatUrl("{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries/entry?*")),
+            pattern: new URLPattern(
+                rest.formatUrl(
+                    "{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries/entry?*",
+                ),
+            ),
             limitations: ["Authenticated"],
-            duration: 60000,
+            duration: 60_000,
             count: 300,
         },
         // Increment the value of a DataStore entry
         {
             methods: ["POST"],
-            pattern: new dntShim.URLPattern(rest.formatUrl("{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries/entry/increment?*")),
+            pattern: new URLPattern(
+                rest.formatUrl(
+                    "{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries/entry/increment?*",
+                ),
+            ),
             limitations: ["Authenticated"],
-            duration: 60000,
+            duration: 60_000,
             count: 300,
         },
         // Delete DataStore entry
         {
             methods: ["DELETE"],
-            pattern: new dntShim.URLPattern(rest.formatUrl("{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries/entry?*")),
+            pattern: new URLPattern(
+                rest.formatUrl(
+                    "{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries/entry?*",
+                ),
+            ),
             limitations: ["Authenticated"],
-            duration: 60000,
+            duration: 60_000,
             count: 300,
         },
         // List DataStore entry versions
         {
             methods: ["GET"],
-            pattern: new dntShim.URLPattern(rest.formatUrl("{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries/entry/versions?*")),
+            pattern: new URLPattern(
+                rest.formatUrl(
+                    "{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries/entry/versions?*",
+                ),
+            ),
             limitations: ["Authenticated"],
-            duration: 60000,
+            duration: 60_000,
             count: 300,
         },
         // Get DataStore entry version
         {
             methods: ["GET"],
-            pattern: new dntShim.URLPattern(rest.formatUrl("{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries/entry/versions/version?*")),
+            pattern: new URLPattern(
+                rest.formatUrl(
+                    "{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores/datastore/entries/entry/versions/version?*",
+                ),
+            ),
             limitations: ["Authenticated"],
-            duration: 60000,
+            duration: 60_000,
             count: 300,
         },
         // List DataStores
         {
             methods: ["GET"],
-            pattern: new dntShim.URLPattern(rest.formatUrl("{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores?*")),
+            pattern: new URLPattern(
+                rest.formatUrl(
+                    "{BEDEV2Url:datastores}/v1/universes/:universeId/standard-datastores?*",
+                ),
+            ),
             limitations: ["Authenticated"],
-            duration: 60000,
+            duration: 60_000,
             count: 300,
-        },
+        },*/
     ]);
 }
